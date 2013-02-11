9c12a2009e837c82f4f69924d5221965c98aecf3
Walter Lapchynski <carsrcoffins23@yahoo.com>
2013-02-10 18:52:09 -0800

    regex identifier validation

--- uploadit.sh
+++ uploadit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# uploadit v1.1
+# uploadit v1.2
 # author: walter lapchynski/wxl
 # contact: carsrcoffins23@yahoo.com
 # license: public domain
@@ -12,17 +12,25 @@
 # first let's get our cookie
 curl --location --dump-header cookies http://archive.org/account/login.php >/dev/null 2>&1
 
-# let's get out login info
+# let's get our login info
 read -e -p "username: " username
 read -es -p "password: " password
+
+# add a newline because read -s doesn't end in one
 echo -e "\n"
 
 # now let's get logged in, making sure to get a new cookie
 curl --location --dump-header cookies2 --cookie cookies --data-urlencode "username=${username}" --data-urlencode "password=${password}" --data-urlencode "remember=CHECKED" --data-urlencode "action=login" --data-urlencode "submit=Log in" http://archive.org/account/login.php >/dev/null 2>&1
 
 # we need an identifier
-### TOFIX: make sure to check if it's valid
 read -e -p "identifier: " identifier
+
+# validate identifier: 5-50 characters, alphanumeric 1st, rest alphanumeric plus period or underscore
+until [[ $identifier =~ ^[A-Za-z0-9][A-Za-z0-9\._]{4,49}$ ]]
+	do
+		echo "invalid identifier. must be 5-50 alphanumeric characters, with . or _ allowed after the first character"
+		read -e -p "enter new identifier: " identifier
+	done
 
 # then let's get our identifier
 curl --location --cookie cookies2 --data "ftp=1&identifier=${identifier}&submit=Create%20item%21" http://archive.org/create.php >/dev/null 2>&1
