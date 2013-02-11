09911566d6b3047faacda3d7a8fa48ee54eecf82
Walter Lapchynski <carsrcoffins23@yahoo.com>
2013-02-10 16:29:10 -0800

    quiet output, support for multiple filenames

--- uploadit.sh
+++ uploadit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# uploadit v1.0
+# uploadit v1.1
 # author: walter lapchynski/wxl
 # contact: carsrcoffins23@yahoo.com
 # license: public domain
@@ -10,42 +10,50 @@
 # make sure you have cURL!
 
 # first let's get our cookie
-curl --location --dump-header cookies http://archive.org/account/login.php
+curl --location --dump-header cookies http://archive.org/account/login.php >/dev/null 2>&1
 
 # let's get out login info
 read -e -p "username: " username
 read -es -p "password: " password
+echo -e "\n"
 
 # now let's get logged in, making sure to get a new cookie
-curl --location --dump-header cookies2 --cookie cookies --data-urlencode "username=${username}" --data-urlencode "password=${password}" --data-urlencode "remember=CHECKED" --data-urlencode "action=login" --data-urlencode "submit=Log in" http://archive.org/account/login.php
+curl --location --dump-header cookies2 --cookie cookies --data-urlencode "username=${username}" --data-urlencode "password=${password}" --data-urlencode "remember=CHECKED" --data-urlencode "action=login" --data-urlencode "submit=Log in" http://archive.org/account/login.php >/dev/null 2>&1
 
 # we need an identifier
 ### TOFIX: make sure to check if it's valid
 read -e -p "identifier: " identifier
 
 # then let's get our identifier
-curl --location --cookie cookies2 --data "ftp=1&identifier=${identifier}&submit=Create%20item%21" http://archive.org/create.php
+curl --location --cookie cookies2 --data "ftp=1&identifier=${identifier}&submit=Create%20item%21" http://archive.org/create.php >/dev/null 2>&1
 
-# we need a filename
-### TOFIX: do more than one with a loop, possible curl ftp
-read -e -p "filename: " filename
+# we need a filenames and will use loops and arrays to get multiples, if necessary
+# get first member of the array
+read -e -p "enter filename or ENTER when done: " filename[0]
 
-# upload
-ftp -in <<EOF
+# loop while checking last element of array for null
+until [ -z ${filename[`expr ${#filename[@]} - 1`]} ]
+	do
+		# get a new last element
+		read -e -p "enter filename or ENTER when done: " filename[${#filename[@]}]
+	done
+
+# upload making sure not to upload our last element of the array-- the null
+ftp -in >/dev/null 2>&1 <<EOF
 open items-uploads.archive.org
 user ${username} ${password}
 cd ${identifier}
 bin
-put ${filename}
+mput ${filename[@]:0:$(expr ${#filename[@]} - 1)}
 close
 bye
 EOF
 
 # check-in the identifier
-curl --location --cookie cookies2 "http://archive.org/checkin/${identifier}"
+curl --location --cookie cookies2 "http://archive.org/checkin/${identifier}" >/dev/null 2>&1
 
 # navigate to the identifier and set the mediatype
-curl --location --cookie cookies2 "http://archive.org/editxml.php?field_default_mediatype=audio&field_default_collection=opensource_audio&edit_item=${identifier}&type=audio&submit=Submit%20audio"
+curl --location --cookie cookies2 "http://archive.org/editxml.php?field_default_mediatype=audio&field_default_collection=opensource_audio&edit_item=${identifier}&type=audio&submit=Submit%20audio" >/dev/null 2>&1
 
 ### TOFIX: should be redirected to metadata page
 
